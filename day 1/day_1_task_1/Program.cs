0ca33658bbbe1c16147036970c5718d613b186bc
OlimovFurqat03 <134360890+OlimovFurqat03@users.noreply.github.com>
2023-06-06 14:51:22 +0300

    Add files via upload

---
+++ day 1/day_1_task_1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+string filePath = @"store.txt";
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.Unicode;
+
+while (true)
+{
+    string[] products = File.ReadAllLines("store.txt");
+    Console.WriteLine(@"Выберите действие:
+a) Показать список продуктов
+b) Добавить новый продукт
+с) Продать продукт");
+
+    string action = Console.ReadLine();
+    if (action == "a")
+    {
+        foreach (var product in products)
+        {
+            Console.WriteLine(product);
+        }
+    }
+    else if (action == "b")
+    {
+        Console.WriteLine("Вводите имя");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Вводите кол-во");
+        int count = int.Parse(Console.ReadLine());
+
+
+        bool productExists = false;
+        for (int i = 0; i < products.Length; i++)
+        {
+            string product = products[i];
+            var split = product.Split(' ');
+            string nom = split[0];
+            int countOfExisting = int.Parse(split[1]);
+            if (name == nom)
+            {
+                count += countOfExisting;
+                products[i] = $"{name} {count}";
+                File.WriteAllLines("store.txt", products);
+                productExists = true;
+                break;
+            }
+        }
+
+        if (productExists)
+        {
+            string[] newProducts = new string[products.Length + 1];
+            for (int i = 0; i < newProducts.Length - 1; i++)
+            {
+                newProducts[i] = products[i];
+            }
+            newProducts[newProducts.Length - 1] = $"{name} {count}";
+            File.WriteAllLines("store.txt", newProducts);
+        }
+    }
+    else if (action == "c")
+    {
+        Console.WriteLine("Вводите имя");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Вводите кол-во");
+        int count = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < products.Length; i++)
+        {
+            var product = products[i];
+            var split = product.Split(' ');
+            string nom = split[0];
+            int countOfExisting = int.Parse(split[1]);
+            if (name == nom)
+            {
+                countOfExisting -= count;
+                products[i] = $"{name} {countOfExisting}";
+                File.WriteAllLines("store.txt", products);
+                break;
+            }
+        }
+    }
+}
---
+++ day 1/day_1_task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace First_prog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b=");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c=");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int dic = b * b - (4 * a * c);
+            
+        if (dic > 0)
+        {
+            double x1 = (-b - Math.Sqrt(dic)) / (2 * a);
+            double x2 = (-b + Math.Sqrt(dic)) / (2 * a);
+            Console.WriteLine("x1=" + x1);
+            Console.WriteLine("x2=" + x2);
+        }
+            if (dic == 0)
+            {
+                int x = (-b / (2 * a));
+                Console.WriteLine("x=" + x);
+            }
+                if (dic < 0)    
+                {
+                    Console.WriteLine("У этого уровнения нет решения потому что дискриминант меньще 0");
+                }
+        }
+    }
+}
